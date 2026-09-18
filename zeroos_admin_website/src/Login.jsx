7baa73709356dedd342d572f206fcc1c3c1f8141
Joshua Stahl <47959058+joshuadstahl@users.noreply.github.com>
2026-09-18 11:10:04 -0700

    text 3

--- zeroos_admin_website/src/Login.jsx
+++ zeroos_admin_website/src/Login.jsx
@@ -1,12 +1,12 @@
 import { useStytchSession } from "@stytch/react";
-import { Navigate } from "react-router";
+//import { Navigate } from "react-router";
 import { LoginOrSignup } from "./LoginOrSignup";
 
 export const Login = () => {
   const { session } = useStytchSession();
 
   if (session) {
-    return <Navigate to="/home" />;
+    return <h1>Welcome back!</h1>;
   }
 
   return <LoginOrSignup />;
