2ff2cf0f9a2e6474d9bb5dbae92bd1b71ed64744
Joshua Stahl <47959058+joshuadstahl@users.noreply.github.com>
2026-09-18 11:25:36 -0700

    test 6

--- zeroos_admin_website/src/main.jsx
+++ zeroos_admin_website/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {createStytchClient, StytchProvider} from "@stytch/react";
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { Login } from './Login.jsx';
@@ -9,9 +10,11 @@ const stytch = createStytchClient("public-token-test-9a31df33-02d8-466c-8825-fae
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <StytchProvider stytch={stytch}>
-      <App />
-      <Login />
-    </StytchProvider>
+    <BrowserRouter>
+      <StytchProvider stytch={stytch}>
+        <App />
+        <Login />
+      </StytchProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
