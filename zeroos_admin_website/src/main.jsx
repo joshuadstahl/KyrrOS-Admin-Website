719787c97d70d77490b3429dbb1efa713e9d4fca
Joshua Stahl <47959058+joshuadstahl@users.noreply.github.com>
2026-09-18 11:27:08 -0700

    test 7

--- zeroos_admin_website/src/main.jsx
+++ zeroos_admin_website/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import {createStytchClient, StytchProvider} from "@stytch/react";
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router';
 import './index.css'
 import App from './App.jsx'
 import { Login } from './Login.jsx';
