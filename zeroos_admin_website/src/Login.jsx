8a2f67bc0525ef2613fac382bd70fc32e98c6b79
Joshua Stahl <47959058+joshuadstahl@users.noreply.github.com>
2026-09-18 11:22:51 -0700

    test 5

--- zeroos_admin_website/src/Login.jsx
+++ zeroos_admin_website/src/Login.jsx
@@ -6,7 +6,8 @@ export const Login = () => {
   const { session } = useStytchSession();
 
   if (session) {
-    return <Navigate to="/" />;
+    //return <Navigate to="/" />;
+    return <h1>Already logged in</h1>;
   }
 
   return <LoginOrSignup />;
